918bd07da6d19d6fb80a103b32da6bc6be4addab
Klobetz Ákos <70511274+klobetz@users.noreply.github.com>
2026-09-21 12:36:30 +0200

    #1 alapozás, változók és bekérés kiíratás

---
+++ 00_alapozas.py
@@ -0,0 +1,43 @@
+a = 2
+
+print(a)
+
+b="szöveg"
+
+print(b)
+
+b=11
+
+print(b)
+
+print("teszt1")
+
+c = a+b
+
+print(c)
+
+print(b//a)
+print(b//a)
+print(b%a)
+print(b**a)
+
+
+szam = 6
+if szam % 2 == 0 :
+    print ("a szám páros")
+else:
+    print ("a szám páratlan")
+
+#**********************************************************    
+bekeres = input("kérek valamit:")
+
+print (bekeres)
+
+szam2 = (input("kérek egy számot:"))
+if szam2 % 2 == 0 :
+    print ("a szám páros")
+else:
+    print ("a szám páratlan")
+
+
+print("teszt")
