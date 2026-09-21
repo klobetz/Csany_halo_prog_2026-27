9223d9107e368126cc87b74daed23a6e2142c712
klobetz <klobetz@gmail.com>
2026-09-21 13:38:21 +0200

    új formázott kiíratás és adatok bekérése, gyakorlás

---
+++ 00-1-gyakorlas.py
@@ -0,0 +1,8 @@
+#tabuló neve, osztálya, szak, kedvenc tantárgy, hány órája van egy héten
+name = input("Kérem a tabuló nevét: ")
+class_name = input("Kérem az osztályt: ")
+specialty = input("Kérem a szakot: ")
+favorite_subject = input("Kérem a kedvenc tantárgyat: ")
+weekly_hours = int(input("Kérem, hány órája van egy héten: "))
+
+print(f"Tabuló neve: {name}, Osztály: {class_name}, Szak: {specialty}, Kedvenc tantárgy: {favorite_subject}, Hány órája van egy héten: {weekly_hours}")
--- 00_alapozas.py
+++ 00_alapozas.py
@@ -33,11 +33,34 @@ bekeres = input("kérek valamit:")
 
 print (bekeres)
 
+<<<<<<< HEAD
 szam2 = (input("kérek egy számot:"))
+=======
+szam2 = int(input("kérek egy számot:"))
+>>>>>>> 5f722b4 (új formázott kiíratás és adatok bekérése, gyakorlás)
 if szam2 % 2 == 0 :
     print ("a szám páros")
 else:
     print ("a szám páratlan")
 
 
+<<<<<<< HEAD
 print("teszt")
+=======
+name = "Kata"
+age = 20
+height = 1.70
+
+print("A nevem: ", name, "Az életkorom: ", age, "A magasságom: ", height)
+print(f"neven: {name}, életkor: {age}, magasság: {height} cm")
+
+
+#kérj be adatot : teljes név, életkor, születési dátum, lakcím, telefonszám, e-mail cím
+full_name = input("Kérem a teljes nevét: ")
+age = int(input("Kérem az életkorát: "))
+birth_date = input("Kérem a születési dátumát (ÉÉÉÉ-HH-NN): ")
+address = input("Kérem a lakcímét: ")
+phone_number = input("Kérem a telefonszámát: ")
+email = input("Kérem az e-mail címét: ")
+print(f"Teljes név: {full_name}, Életkor: {age}, Születési dátum: {birth_date}, Lakcím: {address}, Telefonszám: {phone_number}, E-mail cím: {email}")
+>>>>>>> 5f722b4 (új formázott kiíratás és adatok bekérése, gyakorlás)
